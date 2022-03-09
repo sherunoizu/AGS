23ffbface26ff1209f291119e672b1367809616d
Nikita <nikitazapletin@gmail.com>
2022-03-09 15:43:09 +0300

    instaFeed setup

--- dist/js/script.js
+++ dist/js/script.js
@@ -166,17 +166,34 @@ function flipCard() {
 }
 flipCard();
 
+function toggleHeader(humburger) {
+    const hidingElement = document.querySelectorAll(`.hiding`);
 
+    if (humburger.classList.contains(`active`)) {
+        for (let i = 0; i < hidingElement.length; i++) {
+            // hidingElement[i].style.opacity = '0';
+            hidingElement[i].style.opacity = '0';
+        }
+    } else {
+        for (let i = 0; i < hidingElement.length; i++) {
+            hidingElement[i].style.opacity = '1';
+        }
+    }
+
+    
+}
 
 
 
 function menu() {
     const humburger = document.querySelector(`.humburger`);
     const overlay = document.querySelector(`.overlay`);
+    const menu = document.querySelector(`.header__menu-item`);
+    console.log(menu);
     humburger.addEventListener(`click`, () => {
         humburger.classList.toggle(`active`);
         overlay.classList.toggle(`active`);
-        // hideMenu(humburger, overlay);
+        toggleHeader(humburger);
         return;
     });
     overlay.addEventListener(`click`, () => {
